79cdb4f354cc398b09a3ae9ac117b37e7ffe7a19
Kristoffer Carlsson <kcarlsson89@gmail.com>
2013-06-05 16:27:44 +0200

    Added dictionary for converting element names
    Different FE softwares uses different names for the same element. This
    file is to facilitate converting between them.

---
+++ phon/io/element_name_dictionary.py
@@ -0,0 +1,6 @@
+element_2d_dictionary =  {("CPE3", "abaqus") : "CPE3",
+                          ("CPE3", "oofem") : "TrplaneStrain"}
+                          
+
+element_3d_dictionary = {("C3D4", "abaqus") : "C3D4",
+                         ("C3D4", "oofem") : "LTRSpace"}
