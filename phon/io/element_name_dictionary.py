de14c09ab578151515ab3c014c9896cef410ae70
Kristoffer Carlsson <kcarlsson89@gmail.com>
2013-06-06 20:51:09 +0200

    Added inverse element dictionary and 2d/3d class.

--- phon/io/element_name_dictionary.py
+++ phon/io/element_name_dictionary.py
@@ -1,6 +1,13 @@
-element_2d_dictionary =  {("CPE3", "abaqus") : "CPE3",
-                          ("CPE3", "oofem") : "TrplaneStrain"}
-                          
+element_dictionary =  {("CPE3", "abaqus") : "CPE3",
+                       ("CPE3", "oofem") : "TrplaneStrain",
+                       ("C3D4", "abaqus") : "C3D4",
+                       ("C3D4", "oofem") : "LTRSpace"}
 
-element_3d_dictionary = {("C3D4", "abaqus") : "C3D4",
-                         ("C3D4", "oofem") : "LTRSpace"}
+element_dictionary_inverse  =  {("CPE3", "abaqus") : "CPE3",
+                                ("TrplaneStrain", "oofem") : "CPE3",
+                                ("C3D4", "abaqus") : "C3D4",
+                                ("LTRSpace", "oofem") : "C3D4"}
+        
+
+elements_2d = ["CPE3"]
+elements_3d = ["C3D4"]
