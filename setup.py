da2c5c04cfa3bdfcf62735295169702594c30d9a
Kristoffer Carlsson <kcarlsson89@gmail.com>
2013-06-16 14:58:58 +0200

    Updated installation files.

--- setup.py
+++ setup.py
@@ -6,16 +6,14 @@ import phon
 
 
 setup(name='phon',
-      version='0.1',
+      version='0.1.0',
       author='Kristoffer Carlsson',
       author_email='kcarlsson89@gmail.com',
       url='https://github.com/KristofferC/phon',
       download_url='https://github.com/KristofferC/phon',
       description='Insertion of cohesive elements between polyhedral grains',
       long_description=open('README.rst').read(),
-      package_dir={'': 'phon'},
-      py_modules=['phon'],
-      provides=['phon'],
+      packages=['phon', 'phon.io', 'phon.mesh_objects', 'phon.mesh_tools'],
       keywords='neper voronoi mesh cohesive oofem polycrystalline',
       license='The MIT License (MIT)',
       classifiers=['Development Status :: 2 - Pre-Alpha',
