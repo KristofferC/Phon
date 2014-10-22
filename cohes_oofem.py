f3aa5925a5c376ae9ea4d1347a0afd0910597146
Mikael Öhman <micketeer@gmail.com>
2014-10-22 10:11:10 +0200

    More fixes (closer to working gmsh input)

--- cohes_oofem.py
+++ cohes_oofem.py
@@ -8,8 +8,13 @@ from phon.mesh_tools.create_cohesive_elements import create_cohesive_elements
 
 inputfile = sys.argv[1]
 scale = float(sys.argv[2])
+nfiles = 1
+if len(sys.argv) >= 4:
+    nfiles = int(sys.argv[3])
+    mesh = read_from_gmsh_inp(inputfile, nfiles, verbose=0)
+else:
+    mesh = read_from_neper_inp(inputfile, verbose=0)
 
-mesh = read_from_neper_inp("test/n10-id1.inp", verbose=0)
 create_cohesive_elements(mesh)
 mesh.renumber_nodes()
 
