e95d8acfbd5a6df6b3bda585fb8bee6aaebcec2e
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-01-26 14:20:47 +0100

    Changed to setuptools.
    Added version check for Python.

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+
 import sys
+
+
+# Make sure we have the right Python version.
+if sys.version_info[:2] < (2, 7):
+    print("Phon requires Python 2.7 or newer. Python %d.%d detected" % sys.version_info[:2])
+    sys.exit(-1)
 
 sys.path.append('phon')
 
@@ -8,17 +15,25 @@ setup(name='phon',
       author='Kristoffer Carlsson',
       author_email='kristoffer.carlsson@chalmers.se',
       url='https://github.com/KristofferC/phon',
-      download_url='https://github.com/KristofferC/phon',
-      description='Insertion of cohesive elements between polyhedral grains',
+      download_url='https://github.com/KristofferC/Phon/tarball/master',
+      description='Insertion of cohesive elements between grains in a mesh.',
       long_description=open('README.md').read(),
       packages=['phon', 'phon.io', 'phon.io.read', 'phon.io.write',
                 'phon.mesh_objects', 'phon.mesh_tools'],
       keywords='neper voronoi mesh cohesive oofem polycrystalline',
       license='The MIT License (MIT)',
-      classifiers=['Development Status :: 4 - Beta',
-                   'Intended Audience :: Developers',
-                   'Natural Language :: English',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 2',
-                   'Topic :: Scientific/Engineering :: GIS']
-      )
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      install_requires=['numpy>=1.9.0']
+)
