fea1d5a85b0e1ed000f2fb20c7860807a55c41e1
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-01-27 18:54:27 +0100

    Added some more description to arguments

--- bin/example_gmsh.py
+++ bin/example_gmsh.py
@@ -1,4 +1,5 @@
 import argparse
+import ntpath
 
 from phon.io.read.read_from_gmsh import read_from_gmsh
 from phon.io.write.export_to_oofem import export_to_oofem
@@ -7,11 +8,13 @@ from phon.mesh_tools.create_cohesive_elements import create_cohesive_elements
 
 parser = argparse.ArgumentParser(description="Imports gmsh .msh files to a mesh, "
                                              "creates cohesive elements "
-                                             "and exports it to either OOFEM or Abaqus.")
+                                             "and exports it to either OOFEM or Abaqus. Mesh files "
+                                             "should each have one .msh file per grain with  the name "
+                                             "'meshfileX.msh' where X is the grain id.")
 
 
 requiredNamed = parser.add_argument_group('required named arguments')
-requiredNamed.add_argument("-b", "--basefilename", help="The basename of the files, everything before the "
+requiredNamed.add_argument("-b", "--basefilename", help="The basename of the files, that is everything before the "
                                                         "grain id and the file extension.", required=True)
 
 requiredNamed.add_argument("-n", "--n_grains", help="The number of total grains, you should have "
@@ -21,13 +24,10 @@ requiredNamed.add_argument('-o', '--output', choices=["abaqus", "oofem"], help='
                            required=True)
 
 parser.add_argument("-s", '--scale', help="Scale to scale all the nodes with.", type=float)
-parser.add_argument('-v', '--verbose', action='count', help="Level of verbosity, -v for level 1, -vv for level 2 etc.")
-
+parser.add_argument('-v', '--verbose', action='count', help="Level of verbosity, -v for level 1, -vv for level 2 etc.",
+                    default=0)
 
 args = parser.parse_args()
-
-if not args.verbose:
-    args.verbose = 0
 
 mesh = read_from_gmsh(args.basefilename, args.n_grains, args.verbose)
 
@@ -40,8 +40,8 @@ if args.scale:
 
 print("Exporting to " + args.output + "...")
 if args.output == "abaqus":
-    export_to_abaqus(args.basefilename + "_coh.inp", mesh, write_2d_elements=False)
+    export_to_abaqus(ntpath.basename(args.basefilename) + "_coh.inp", mesh, write_2d_elements=False)
 if args.output == "oofem":
-    export_to_oofem(args.basefilename + "_coh.in", mesh, write_2d_elements=False)
+    export_to_oofem(ntpath.basename(args.basefilename) + "_coh.in", mesh, write_2d_elements=False)
 
 print("Done!")
