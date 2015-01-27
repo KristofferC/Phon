fd59cf57022aa0e33190cec246b0a195d3d31cf1
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2015-01-27 14:51:42 +0100

    Updated the argument parser

--- bin/example_gmsh.py
+++ bin/example_gmsh.py
@@ -1,30 +1,47 @@
-import sys
+import argparse
 
 from phon.io.read.read_from_gmsh import read_from_gmsh
-from phon.io.read.read_from_abaqus_inp import read_from_abaqus_inp
 from phon.io.write.export_to_oofem import export_to_oofem
 from phon.io.write.export_to_abaqus import export_to_abaqus
 from phon.mesh_tools.create_cohesive_elements import create_cohesive_elements
 
+parser = argparse.ArgumentParser(description="Imports gmsh .msh files to a mesh, "
+                                             "creates cohesive elements "
+                                             "and exports it to either OOFEM or Abaqus.")
 
-inputfile = sys.argv[1]
-scale = float(sys.argv[2])
 
-# inputfile = "../test/n10-id1.inp"
-# scale = 1.0
+requiredNamed = parser.add_argument_group('required named arguments')
+requiredNamed.add_argument("-b", "--basefilename", help="The basename of the files, everything before the "
+                                                        "grain id and the file extension.", required=True)
 
-nfiles = 1
-if len(sys.argv) >= 4:
-    nfiles = int(sys.argv[3])
-    mesh = read_from_gmsh(inputfile, nfiles, verbose=0)
-else:
-    mesh = read_from_abaqus_inp(inputfile, verbose=0)
+requiredNamed.add_argument("-n", "--n_grains", help="The number of total grains, you should have "
+                                                    "one .msh file for each grain.", required=True, type=int)
 
-create_cohesive_elements(mesh, 3)
+requiredNamed.add_argument('-o', '--output', choices=["abaqus", "oofem"], help='Software to export the mesh to.',
+                           required=True)
+
+parser.add_argument("-s", '--scale', help="Scale to scale all the nodes with.", type=float)
+parser.add_argument('-v', '--verbose', action='count', help="Level of verbosity, -v for level 1, -vv for level 2 etc.")
+
+
+args = parser.parse_args()
+
+if not args.verbose:
+    args.verbose = 0
+
+mesh = read_from_gmsh(args.basefilename, args.n_grains, args.verbose)
+
+create_cohesive_elements(mesh, mesh_dimension=3)
 
 # Rescale the RVE:
-for node_id, node in mesh.nodes.items():
-    node.c *= scale
+if args.scale:
+    for node_id, node in mesh.nodes.items():
+        node.c *= args.scale
 
-export_to_oofem("n10-id1_coh.in", mesh, False)
-export_to_abaqus("n10-id1_coh.inp", mesh, False)
+print("Exporting to " + args.output + "...")
+if args.output == "abaqus":
+    export_to_abaqus(args.basefilename + "_coh.inp", mesh, write_2d_elements=False)
+if args.output == "oofem":
+    export_to_oofem(args.basefilename + "_coh.in", mesh, write_2d_elements=False)
+
+print("Done!")
